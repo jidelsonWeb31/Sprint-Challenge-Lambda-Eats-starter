e319305bea0785db8c1c8f0a5b2cb2623139ca02
jidelson <58210030+jidelson@users.noreply.github.com>
2020-05-22 14:57:54 -0700

    getting cypress working

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -1,31 +1,30 @@
 describe('Form inputs', function () {
-    
+
+
     it('Checks if I can add text', function(){
         
      cy.visit('index.html')
 
-
-    cy.url()
-    .should('include', 'localhost')
-
-    
+        cy.get('input[name="name"]')
+        .type('Lady Gaga')
     })
 
+    
+
+    //second test
     it('Tests that I can submit the form', function(){
        
         cy.get('button.submit')
     })
+
+
+
+    // third test
+    it('Checks if I can select multiple toppings', function(){
+        
+        cy.get('input[type="checkbox"]')
+    })
 })
 
-
-
-
-// describe('Multiple toppings', function () {
-    
-//     it('Checks if I can select multiple toppings', function(){
-        
-     
-//     })
-// })
 
 
