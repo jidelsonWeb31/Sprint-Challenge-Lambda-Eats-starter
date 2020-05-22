31aa76b033066ff126bd38b9ac6f02ab896fd336
jidelson <58210030+jidelson@users.noreply.github.com>
2020-05-22 15:50:54 -0700

    need to get one more test working

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -7,24 +7,59 @@ describe('Form inputs', function () {
 
         cy.get('input[name="name"]')
         .type('Lady Gaga')
+        .should('have.value', 'Lady Gaga')
     })
 
     
 
-    //second test
-    it('Tests that I can submit the form', function(){
-       
+    // test
+    it('Tests that my button is initially disabled', function(){
         cy.get('button.submit')
+        .should('be.disabled')
     })
 
 
+    //test
+    it('Checks if I can change special instructions', function(){
+        
+           cy.get('input[name="specialInstructions"]')
+           .type('Come up the stairs')
+           .should('have.value', 'Come up the stairs')
+       })
+   
 
-    // third test
+       //test
+it('Checks if I can select a size', function(){
+    cy.get('select[name="size"]')
+    .select('large')
+    .should('have.value', 'large')
+})
+
+    //  test
     it('Checks if I can select multiple toppings', function(){
         
         cy.get('input[type="checkbox"]')
+        .select('sausage')
+        .should('have.value', 'sausage')
+    })
+
+// // test
+// it('Tests that my button submits', function(){
+//     cy.get('button.submit')
+//     .should('not.be.disabled')
+// })
+
+})
+
+describe('Submitting Order', () => {
+    it('can submit an order', () => {
+        cy.visit('index.html')
+        cy.get('input[name="name"]').type('Lady Gaga')
+        cy.get('input[name="specialInstructions"]').type('Come up the stairs')
+        cy.get('select[name="size"]').select('large')
     })
 })
+
 
 
 
